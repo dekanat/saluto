15e59a11322f139cb0127db7e9bc9b32553e3a69
darthspectrum <muradianator@gmail.com>
2020-06-20 02:01:04 +0400

    Fiddled with Gatsby
    Created a simple welcome page

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Saluto
 
 Saluto is supposed to be landing page of dekanat.
+
+This branch is the implementation of Saluto in [Gatsby](https://www.gatsbyjs.org/)
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Dekanat / Դեկանատ" />
+    <h3>Մինչ շուրթերը դատապարտում են, սիրտն արդարացնում է:</h3>
+  </Layout>
+)
+
+export default IndexPage
